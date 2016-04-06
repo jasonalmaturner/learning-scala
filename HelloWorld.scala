73203d73718dea0fc8e994175ba6a44068491158
Jason <jasonalmaturner@gmail.com>
2016-04-05 21:44:05 -0600

    starting up again

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,5 +1,32 @@
 object HelloWorld {
+  val me : String = "Jason Turner";
   def main (args: Array[String]) {
-    println("HelloWorld")
+    println(me)
+    things.sayName();
+    things.loopName();
+    println(addStuff.addInt(5, 6));
+  }
+}
+
+object things {
+  val disMe : String = "Jason";
+  def sayName () {
+    println(disMe);
+  }
+
+  def loopName () {
+    var i = 0;
+    for (i <- 0 to 10) {
+      println(disMe + " " + i);
+    }
+  }
+}
+
+object addStuff {
+  def addInt(a:Int = 0, b:Int = 0) : Int = {
+    var sum:Int = 0;
+    sum = a + b;
+
+    return sum;
   }
 }
